f06b2a7be91dc2a4f57fa8d269ab86bc5fc589fe
chinthakaak <chinthakaak@gmail.com>
2015-03-25 21:13:16 +0530

    proxy addition

--- bin/configure-proxy.sh
+++ bin/configure-proxy.sh
@@ -7,4 +7,4 @@
 
 echo Configuring proxy to $proxyserver:$proxyport $proxyenabled
 cp conf/original/settings.xml conf/
-sed -i 's/@proxyserver@/'$proxyserver'/g; s/@proxyport@/'$proxyport'/g; s/@proxyenabled@/'$proxyenabled'/g;' conf/settings.xml
+sed -i 's/@proxyserver@/'$proxyserver'/g; s/@proxyport@/'$proxyport'/g; s/@proxyenabled@/'$proxyenabled'/g; s/@bypassproxy@/'$bypassproxy'/g;' conf/settings.xml
